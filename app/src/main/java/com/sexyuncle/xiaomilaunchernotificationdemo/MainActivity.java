20659b2782effb4f98b4bf14767f8d4d685c5914
SexyUncle <devsexyuncle@gmail.com>
2016-04-21 18:47:28 +0800

    完善

--- app/src/main/java/com/sexyuncle/xiaomilaunchernotificationdemo/MainActivity.java
+++ app/src/main/java/com/sexyuncle/xiaomilaunchernotificationdemo/MainActivity.java
@@ -2,10 +2,6 @@ package com.sexyuncle.xiaomilaunchernotificationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Utils.getFiledOfClass(MainActivity.this, 10000);
-        final EditText editText = (EditText) findViewById(R.id.number);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = editText.getText().toString();
-                if (!TextUtils.isEmpty(text)) {
-                    Utils.getFiledOfClass(MainActivity.this, Integer.parseInt(text));
-                }
-            }
-        });
+        Utils.sendMiuiAppUnreadNotificationNumber(getApplicationContext(),100000);
     }
 }
--- app/src/main/java/com/sexyuncle/xiaomilaunchernotificationdemo/Utils.java
+++ app/src/main/java/com/sexyuncle/xiaomilaunchernotificationdemo/Utils.java
@@ -3,6 +3,12 @@ package com.sexyuncle.xiaomilaunchernotificationdemo;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.lang.reflect.Field;
 
@@ -13,38 +19,78 @@ public class Utils {
 
     private static final String TAG = Utils.class.getSimpleName();
 
-    public static void getFiledOfClass(Context context,int count){
-        try {
-            //miui6以上
-            Notification notification = null;
+    private static final String XIAOMI_MANUFACTURER="Xiaomi";//小米硬件标示
+
+
+    /**
+     *
+     * @param context
+     * @param count 未读消息数目
+     */
+    public static void sendMiuiAppUnreadNotificationNumber(Context context,int count) {
+        if (Build.MANUFACTURER.equals(XIAOMI_MANUFACTURER)) {
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             Notification.Builder builder = new Notification.Builder(context);
-            builder.setContentTitle("你有"+count+"条未读消息");
-            builder.setTicker("你有"+count+"条未读消息");
+            builder.setContentTitle("你有" + count + "条未读消息");
+            builder.setTicker("你有" + count + "条未读消息");
             builder.setAutoCancel(true);
             builder.setSmallIcon(R.mipmap.ic_launcher);
             builder.setDefaults(Notification.DEFAULT_LIGHTS);
-            notification = builder.build();
-            Class clazz = Class.forName("android.app.MiuiNotification");
-            if (clazz!=null){
-                Object miuiNotification = clazz.newInstance();
-                Field messageCount = clazz.getDeclaredField("messageCount");
-                messageCount.setAccessible(true);
-                messageCount.set(miuiNotification,count);
-                Field extraNotification= notification.getClass().getField("extraNotification");
-                extraNotification.setAccessible(true);
-                extraNotification.set(notification, miuiNotification);
-                NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                manager.notify(0,notification);
-            }
+            Notification notification  = builder.getNotification();
+            try {
+                //miui6以上
+                Class clazz = Class.forName("android.app.MiuiNotification");
+                if (clazz != null) {
+                    Object miuiNotification = clazz.newInstance();
+                    Field messageCount = clazz.getDeclaredField("messageCount");
+                    messageCount.setAccessible(true);
+                    messageCount.set(miuiNotification, count);
+                    Field extraNotification = notification.getClass().getField("extraNotification");
+                    extraNotification.setAccessible(true);
+                    extraNotification.set(notification, miuiNotification);
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+                }
+
+            } catch (Exception e) {
+                //miui6以下
+                e.printStackTrace();
+                Intent localIntent = new Intent(
+                        "android.intent.action.APPLICATION_MESSAGE_UPDATE");
+                localIntent.putExtra(
+                        "android.intent.extra.update_application_component_name",
+                        context.getPackageName() + "/" + getLauncherClassName(context));
+                localIntent.putExtra(
+                        "android.intent.extra.update_application_message_text", String.valueOf(count == 0 ? "" : count));
+                context.sendBroadcast(localIntent);
+            }finally {
+                manager.notify(0, notification);
+            }
+        }else {
+            Toast.makeText(context,"这不是小米的手机",Toast.LENGTH_LONG).show();
         }
     }
+
+    /**
+     *
+     * @param context 上下文
+     * @return 返回类名
+     */
+    private static String getLauncherClassName(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setPackage(context.getPackageName());
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        ResolveInfo info = packageManager
+                .resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+
+        if (info == null) {
+            info = packageManager.resolveActivity(intent, 0);
+        }
+
+        return info.activityInfo.name;
+    }
+
+
 }
